832d1f534ed67901552ac76fd7e81977d401b578
LATIFA-ouamou <latifaouamou4444@gmail.com>
2025-11-29 12:47:10 +0100

    Route react

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,21 @@
+import Footer from "./components/Footer"
+import Nav from "./components/Nav"
 
-
+import { Routes, Route } from "react-router-dom";
 function App() {
 
   return (
     <>
-      <h1 className='text-amber-400'>HELLOOO</h1>
+    <Nav/>
+<h1>hhhh</h1>
+     <Routes>
+
+        <Route path="/" element={<h1>Accueil</h1>} />
+        <Route path="/livres" element={<h1>Liste des livres</h1>} />
+        <Route path="/auteurs" element={<h1>Auteurs</h1>} />
+        <Route path="/contact" element={<h1>Contact</h1>} />
+      </Routes>
+      <Footer/>
     </>
   )
 }
---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+    
+      <h1>HELLO footer</h1>
+
+
+    </>
+  )
+}
+
+export default Footer
