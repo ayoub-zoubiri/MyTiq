ebc3d2618f10350a9735f45d67e562eb9e86b024
LATIFA-ouamou <latifaouamou4444@gmail.com>
2025-12-05 11:33:17 +0100

    UPDATE

--- Backend/resources/views/emails/newsletter.blade.php
+++ Backend/resources/views/emails/newsletter.blade.php
@@ -4,8 +4,18 @@
     <title>Confirmation Newsletter</title>
 </head>
 <body>
+   
+    
     <h1>Merci pour votre abonnement ayoub latifa wijdane   !</h1>
+
     <p>Vous êtes maintenant inscrit(e) à notre newsletter.</p>
+
     <p>Cordialement,<br>{{ config('app.name') }}</p>
+
 </body>
 </html>
+
+
+
+
+
