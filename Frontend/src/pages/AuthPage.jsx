7a011df68cc5d9ec886247c46ad973e2a36a9985
Ayoub Zoubiri <ayoubzoubiriyc@gmail.com>
2025-12-10 14:32:15 +0100

    fix

--- Frontend/src/pages/AuthPage.jsx
+++ Frontend/src/pages/AuthPage.jsx
@@ -12,7 +12,7 @@ const AuthPage = () => {
   return (
     
     <div className=" min-h-screen bg-black text-white flex items-center justify-center p-4">
-      
+       
      
       <div className="w-full max-w-md bg-gray-900 p-10 rounded-xl shadow-2xl border border-white border-opacity-10">
         
